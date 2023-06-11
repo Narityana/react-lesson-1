c32d7f7737c06797497b23e6174c1ce110dc01b0
TetiPalii <TetianaPalii9491@gmail.com>
2023-06-11 15:46:16 +0200

    task4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,6 +11,7 @@ import {
 import data from 'data/data';
 import article from 'data/article.json';
 import forbes from 'data/forbes.json';
+import transaction from 'data/transactions';
 
 export const App = () => {
   return (
@@ -42,7 +43,7 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 4
         </Heading>
-        <CryptoHistory />
+        <CryptoHistory items={transaction} />
       </Container>
     </Section>
   );
--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,43 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { formatData } from 'helpers/formatData';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <BaseTable>
+      <THead>
+        <tr>
+          <Th>№</Th>
+          <Th>PRICE</Th>
+          <Th>AMOUNT</Th>
+          <Th>DATE</Th>
+        </tr>
+      </THead>
+
+      <tbody>
+        {items.map(({ id, price, amount, date }, idx) => {
+          return (
+            <Tr key={id}>
+              <Td>{idx + 1}</Td>
+              <Td>{price}</Td>
+              <Td>{amount}</Td>
+              <Td>{formatData(date)}</Td>
+            </Tr>
+          );
+        })}
+      </tbody>
+    </BaseTable>
+  );
 };
+
+// // <Tr>
+// <Td>1</Td>
+// <Td>3190</Td>
+// <Td>0.24843</Td>
+// <Td>02/01/2022, 1:14 PM</Td>
+// </Tr>
+// <Tr>
+// <Td>2</Td>
+// <Td>3195</Td>
+// <Td>0.088</Td>
+// <Td>05/09/2022, 2:24 PM</Td>
+// </Tr>
