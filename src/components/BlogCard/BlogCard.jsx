8a49442776fcbb4150b5402205bc16d9aaac836b
Danyashe <27shdaniil@gmail.com>
2023-06-11 15:13:37 +0300

    task-1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -13,7 +13,36 @@ import {
   UserName,
   Date,
 } from './BlogCard.styled';
+import { formatDistance } from 'helpers/formatDistanceToNow';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  userName,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={userName} />
+          <UserInfo>
+            <UserName>{userName}</UserName>
+            <Date>{formatDistance(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
